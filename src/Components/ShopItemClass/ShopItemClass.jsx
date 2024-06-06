c6b03b9d830ec7ffb9967d4ea911951c78084133
VoynovDev <vladimir.voynov2001@mail.ru>
2024-06-06 18:09:32 +0300

    first commit

---
+++ src/Components/ShopItemClass/ShopItemClass.jsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import '/src/App.css';
+
+export class ShopItemClass extends Component {
+	render() {
+		const {brand, title, description, descriptionFull, currency, price} = this.props.item
+		return (
+			<div className="main-content">
+				<h2>{brand}</h2>
+				<h1>{title}</h1>
+				<h3>{description}</h3>
+				<div className="description">{descriptionFull}</div>
+				<div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+				<div className="divider"></div>
+				<div className="purchase-info">
+					<div className="price">{currency}{price.toFixed(2)}</div>
+					<button>Добавить в корзину</button>
+				</div>
+			</div>
+		);
+	}
+} 
